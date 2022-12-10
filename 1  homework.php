704af8e96f1686f0abb1a1d84342702419d38243
fifsky <fifsky@gmail.com>
2022-12-10 19:35:29 +0800

    今天是我上编程课的第一天，我很高兴，不仅学会了什么是变量，还学会了如何用变量进行计算，我会继续跟爸爸学习编程的！

---
+++ 1  homework.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: 蔡旭东 caixudong@verystar.cn
+ * Date: 2022/12/10 19:13
+ */
+ 
+$a=2;
+$b=5;
+$c=4;
+$d=1;
+
+echo ($a*$b)+$c-$d;
+
+echo "<br>"; //换行
+
+
+echo "very good";
